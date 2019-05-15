f88b7356ae01c9fe24d22195f82f9a6477af422a
Maroof Ali <maroof.cyber@yahoo.com>
2019-05-15 07:37:56 +0500

    Capture a video frame by webcam,display captured frame and print frame data

---
+++ 2.Getting Started with Videos/videoCapture.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+videoObject = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = videoObject.read()
+
+    # for checking return and frame values 
+
+    print(ret)       # it should reurn true if frame is read correctly.
+    print(frame)     # print data contained in a frame
+
+    # Operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #display frame
+    cv2.imshow("FRAME", gray)
+    
+    if cv2.waitKey(0):
+        break
+
+videoObject.release()
+cv2.destroyAllWindows()
