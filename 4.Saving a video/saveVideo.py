f2e1e02ad8627f90a8e520f2973ad145787b6eb5
Maroof Ali <maroof.cyber@yahoo.com>
2019-05-16 07:11:32 +0500

    How to capture a video stream and save it in a file

---
+++ 4.Saving a video/saveVideo.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+videoObject = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640, 480)) #(filename, codec, fps, resolution)
+
+while(videoObject.isOpened()):
+    ret, frame = videoObject.read()
+    
+    if ret == True:
+        #frame = cv2.flip(frame,0)
+
+        print(frame)        #prints captured video stream on console
+        out.write(frame)
+
+        cv2.imshow('Frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+
+
+videoObject.release()
+cv2.destroyAllWindows()
