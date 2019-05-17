e7ed793e621f97ebef5f4f0acb3c596c3e03b7fd
Maroof Ali <maroof.cyber@yahoo.com>
2019-05-17 05:47:58 +0500

    Rectangle,line,circle,text,ellipse and polygon

--- 5.Drawing Functions/drawing.py
+++ 5.Drawing Functions/drawing.py
@@ -16,7 +16,18 @@ img = cv2.circle(img,(447,200), 63, (0,0,255), -1)      # -1 for filling the cir
 
 #adding text to images
 #(image,text,bottom-left corner,font-family,font-size,color,line-type)
-font = cv2.putText(img,"Maroof Ali : OpenCV", (10,500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),2,cv2.LINE_AA)
+img = cv2.putText(img,"Maroof Ali : OpenCV", (10,500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),2,cv2.LINE_AA)
+
+#adding ellipse
+#(image,center of ellipse,length of major and minor axis,angle,color,thickness)
+img = cv2.ellipse(img,(490,350),(100,50),0,0,360,(255,255,255),3)
+
+#adding a polygon
+    #create array of points, shape (rows,1,2)
+    #(image,array of points, True or False,color)
+points =  np.array([[15,10],[20,30],[70,20],[50,10]], np.int32)
+points = points.reshape((-1,1,2))
+img = cv2.polylines(img,[points],True,(0,255,200)) #True for close shape, False for open polygon
 
 
 cv2.imshow("IMAGE with Line", img)
